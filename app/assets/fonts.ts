66f586a4fd6c2fa1b26885ba86125af60097e561
Qayr <raygowell@gmail.com>
2024-11-05 18:47:16 +0700

    fixing error

--- app/assets/fonts.ts
+++ app/assets/fonts.ts
@@ -1,8 +1,12 @@
-import { Inter, Poppins } from 'next/font/google';
+import { Inter, Poppins, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
 export const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 });
