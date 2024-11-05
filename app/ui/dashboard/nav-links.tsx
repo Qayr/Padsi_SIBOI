b59e4ae671c9bff131b503e542a1af261268485a
Qayr <raygowell@gmail.com>
2024-11-05 18:44:33 +0700

    commit ke 2

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -2,7 +2,7 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
-} from '@heroicons/react/24/outline';
+} from '@heroicons/react';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
