cf9cb60ee031ee59ce64555ddd2a9e3d663c6c54
Qayr <126859450+Qayr@users.noreply.github.com>
2024-12-11 14:32:07 +0700

    coba terus

--- app/dashboard/menu/[id]/edit/page.tsx
+++ app/dashboard/menu/[id]/edit/page.tsx
@@ -1,22 +1,26 @@
 import Form from '@/app/dashboard/menu/edit-form';
-import Breadcrumbs from '@/app/dashboard/menu/breadcrums';
+import Breadcrumbs from '../../breadcrums';
 import { fetchMenuById } from '@/app/lib/data';
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
+
+  // Fetch the menu data using the provided id
   const [menu] = await Promise.all([fetchMenuById(id)]);
+
   return (
     <main>
       <Breadcrumbs
         breadcrumbs={[
-          { label: 'Stok', href: '/dashboard/Menu' },
+          { label: 'Stok', href: '/dashboard/Menu' },  // Adjust to the correct path
           {
             label: 'Edit Menu',
-            href: `/dashboard/Menu/${id}/edit`,
-            active: true,
+            href: `/dashboard/Menu/${id}/edit`,  // This URL will point to the current edit page
+            active: true,  // Marks this breadcrumb as the current page
           },
         ]}
       />
+      {/* Pass the fetched menu data to the Form component */}
       <Form menu={menu} />
     </main>
   );
