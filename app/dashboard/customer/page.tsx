60616820ef898eba64ac1cc8922192e23a58f0b1
Qayr <126859450+Qayr@users.noreply.github.com>
2024-12-11 14:15:04 +0700

    page cust

--- app/dashboard/customer/page.tsx
+++ app/dashboard/customer/page.tsx
@@ -1,4 +1,3 @@
-// import React, { useState, useEffect } from 'react';
 import Search from '@/app/assets/search';
 import Table from './table';
 import Pagination from './paginatio';
@@ -14,27 +13,27 @@ export const metadata: Metadata = {
 };
 
 export default async function customersPage({
-  searchParams,
+  searchParams = {},
 }: {
   searchParams?: {
     query?: string;
     page?: string;
   };
 }) {
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
+  const query = searchParams.query || '';
+  const currentPage = Number(searchParams.page) || 1;
 
   const totalPages = await fetchCustomersPages(query);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <h1 className={`${poppins.className} text-2xl `}>Customers</h1>
+        <h1 className={`${poppins.className} text-2xl`}>Customers</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search customers..." />
         <CreateCustomer />
       </div>
-      {/* <Suspense fallback={<loadingCustomersSkeleton />}></Suspense> */}
       <Suspense key={query + currentPage} fallback={<CustomersTableSkeleton />}>
         <Table query={query} CurrentPage={currentPage} />
       </Suspense>
